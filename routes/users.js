ff9a32db8e5332acd9af4879c3cf6247d8000a52
Surya <vkassurya@gmail.com>
2022-09-26 19:21:43 +0530

    Assignment-3

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,19 @@ var authenticate = require('../authenticate');
 
 router.use(bodyParser.json());
 
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content_type', 'application/json');
+      res.json(users);
+    }
+  })
+});
+
+
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
