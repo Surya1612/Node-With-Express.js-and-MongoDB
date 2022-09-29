ce95678bb89c57a255306fce347af92729ea2de2
Surya <vkassurya@gmail.com>
2022-09-29 12:50:58 +0530

    File Upload

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config=require('./config');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promotionRouter = require('./routes/promotionRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter=require('./routes/uploadRouter');
+
 const { error } = require('console');
 
 
@@ -83,6 +87,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promotionRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
